8946c5c4649401c677d1e8f987b5172fe8c90b58
Dharshi <dharshi.fernando@gmail.com>
2019-10-03 21:23:37 +1000

    Add background job to application

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,9 @@ class HomeController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def increment_async
+    ::IncrementCountWorker.perform_async(params[:post_id])
+  end
+
 end
